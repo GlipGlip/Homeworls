5070d578a16c889671269c993cad41a3bc3737c8
Andrew Vdovkin <vdovkin2222@gmail.com>
2024-12-05 15:13:34 +0300

    05/12 2

---
+++ module_1_6.py
@@ -0,0 +1,8 @@
+h_w = 12
+hour = 1.5
+n_c = "Python"
+hh_w = (1.5/12)
+
+# print(h_w), print(hour), print(n_c), print(hh_w)
+
+print("Курс:", n_c, ",", "Всего задач: ", h_w, ",", "Затрачено времени: ", hour, ",", "Среднее время выполнения", hh_w, "." )
