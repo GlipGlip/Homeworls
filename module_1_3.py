e03b439ffb08ca497bb97446a098ab8915dd43a0
Andrew Vdovkin <vdovkin2222@gmail.com>
2024-12-05 14:40:28 +0300

    05/12

---
+++ module_1_3.py
@@ -0,0 +1,11 @@
+name = "Andrew"
+print("Name: " + name)
+
+age = 25
+print("Age: " + str(age))
+
+age = 25+1
+print("New age: " + str(age))
+
+is_student = True
+print("Is Student: " + str(is_student))
